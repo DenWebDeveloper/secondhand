aafccc905be5efc52af8be8269a52c4199f04cd8
Den <denv928@gmail.com>
2018-09-15 18:27:53 +0300

    Adaptive full without footer

--- app/assets/js/bundle.js
+++ app/assets/js/bundle.js
@@ -8,14 +8,32 @@ $(function () {
 });
 
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 5,
+    spaceBetween: 0,
     slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+    },
+    breakpoints: {
+        // when window width is <= 320px
+        340: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            slidesPerGroup: 1,
+        },
+        // when window width is <= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            slidesPerGroup: 2,
+        },
+        // when window width is <= 640px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            slidesPerGroup: 2,
+        }
     }
 });
 },{}]},{},[1]);
