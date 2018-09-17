e94ff668d4b263e2afdef3b29d218159f2c0acd9
windripper <windripper>
2018-09-17 20:12:55 +0300

    about-us, contacts

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
   $.gulp.task('pug', function() {
-    return $.gulp.src('./source/template/index.pug')
+    return $.gulp.src('./source/template/about.pug')
       .pipe($.gp.pug({ pretty: true }))
       .on('error', $.gp.notify.onError(function(error) {
         return {
