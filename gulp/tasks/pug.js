74751a0a789c12bc54aba587b039240d6e39858d
Den <denv928@gmail.com>
2018-09-18 19:57:32 +0300

    Delete dir app with .gitignore

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
   $.gulp.task('pug', function() {
-    return $.gulp.src('./source/template/delivery.pug')
+    return $.gulp.src('./source/template/*.pug')
       .pipe($.gp.pug({ pretty: true }))
       .on('error', $.gp.notify.onError(function(error) {
         return {
